cc1f051dcec544fad82235c188a22d9752aa4df0
Alberto Barrago <albertobarrago@gmail.com>
2024-07-09 16:37:19 +0200

    Update

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from service.connect_mysql import connect_mysql
 from .favicon import route_favicon
 from .quotes import route_external
 
@@ -8,4 +9,6 @@ def create_app():
     app = Flask(__name__, static_folder='../static')
     app.register_blueprint(route_external)
     app.register_blueprint(route_favicon)
+    connect_mysql()
     return app
+
--- service/connect_mysql.py
+++ service/connect_mysql.py
@@ -1,24 +1,34 @@
 # connect_mysql.py
-import string
+from pathlib import Path
 
 import mysql
 from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+config = {
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME'),
+    'raise_on_warnings': True,
+}
 
 
-def get_mysql_connection(host, user, database, password):
+def connect_mysql():
     connection = None
     try:
         connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database
+            host=config.get('host'),
+            user=config.get('user'),
+            password=config.get('password'),
+            database=config.get('database'),
         )
         if connection.is_connected():
-            print(f"Success: Password found - {password}")
-            return password
+            return print(f"Connected to MySQL Server version {connection.get_server_info()}")
     except Error as e:
-        print(f"Failed to connect using password: {password} | Error: {e}")
+        print(f"Failed to connect | Error: {e}")
     finally:
         if connection and connection.is_connected():
             # connection.close()
